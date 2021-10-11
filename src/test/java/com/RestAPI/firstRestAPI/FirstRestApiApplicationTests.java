da831f653508872237501333be76ad6c6a76e63b
Suvrat <suvrat72000@gmail.com>
2021-10-11 22:24:55 +0530

    Initial commit

---
+++ src/test/java/com/RestAPI/firstRestAPI/FirstRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestAPI.firstRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
